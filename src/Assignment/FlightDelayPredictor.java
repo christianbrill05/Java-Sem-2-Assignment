3f8a86fa9976dd5492fa39cd32d12d35c7e5fc17
christianbrill05 <christianbrill2005@gmail.com>
2025-04-22 19:52:09 +0100

    Started FlightDelayPredictor class, initialized variable and caluclated yes and no probabilites for if the flight is delayed or not.

---
+++ src/Assignment/FlightDelayPredictor.java
@@ -0,0 +1,26 @@
+package Assignment;
+
+import java.util.HashMap;
+
+public class FlightDelayPredictor // using Naive Bayes Predictor
+{
+    private double priorYes;
+    private double priorNo;
+    private HashMap<String, Double> yesProbability;
+    private HashMap<String, Double> noProbability;
+
+    // constructor for FlightDelayPredictor
+    public FlightDelayPredictor(double priorYes, double priorNo, HashMap<String, Double> yesProbability, HashMap<String, Double> noProbability)
+    {
+        this.priorYes = 0.485; // from frequency table, yesCount/total = yesProbability -> 97/200 = 0.485
+        this.priorNo = 0.515;// noCount/total = yesProbability -> 103/200 = 0.515
+        this.yesProbability = new HashMap<>();
+        this.noProbability = new HashMap<>();
+    }
+
+    public void Probabilities() // probabilities are hard coded here from frequency table
+    {
+
+    }
+
+}
--- src/Assignment/NaiveBayesPredictor.java
+++
@@ -1,7 +0,0 @@
-package Assignment;
-
-public class NaiveBayesPredictor
-{
-
-
-}

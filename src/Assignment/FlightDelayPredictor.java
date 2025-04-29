998f538d10325ee0212d64e1f36ad36e19d35b5a
christianbrill05 <christianbrill2005@gmail.com>
2025-04-29 13:29:34 +0100

    Finished level 2 method: trainData()

--- src/Assignment/FlightDelayPredictor.java
+++ src/Assignment/FlightDelayPredictor.java
@@ -1,24 +1,130 @@
 package Assignment;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.Scanner;
 
-public class FlightDelayPredictor // using Naive Bayes Predictor
+public class FlightDelayPredictor // using Naive Bayes Predictor Logic
 {
-    private double probYes;
-    private double probNo;
-    private HashMap<String, Double> yesProbability;
-    private HashMap<String, Double> noProbability;
+    private double probYes; // probability of "Yes" in the whole dataset
+    private double probNo;  // probability of "No" in the whole dataset
+    private HashMap<String, Double> yesProbability; // HashMap contains conditional probabilites for "yes"
+    private HashMap<String, Double> noProbability; // contains conditional probabilities for "no"
+    private int yesTotal; // counts the total number of "Yes" in the dataset
+    private int noTotal; // counts the total number of "No" in the dataset
 
     // constructor for FlightDelayPredictor
     public FlightDelayPredictor()
     {
-        this.probYes = 0.485; // from frequency table, yesCount/total = yesProbability -> 97/200 = 0.485
-        this.probNo = 0.515; // noCount/total = yesProbability -> 103/200 = 0.515
+        // this.probYes = 0.485; // from frequency table, yesCount/total = yesProbability -> 97/200 = 0.485 ********hard coded values no longer needed********
+        // this.probNo = 0.515; // noCount/total = yesProbability -> 103/200 = 0.515
         this.yesProbability = new HashMap<>();
         this.noProbability = new HashMap<>();
-        setProbabilities();
     }
 
+    public void trainData(String filename)
+    {
+        HashMap<String, Integer> yesCount = new HashMap<>();
+        HashMap<String, Integer> noCount = new HashMap<>();
+        yesTotal = 0;
+        noTotal = 0;
+        File dataset = new File("FlightIsDelayed Predictive Dataset.csv");
+
+        try (Scanner scanner = new Scanner(dataset))
+        {
+            scanner.nextLine(); // skip the first row that contains the features (DeparturePeriod, Weather etc.)
+
+            while (scanner.hasNextLine())
+            {
+                String[] columns = scanner.nextLine().split(","); // splits line into an array of sub-strings
+
+                String departurePeriod = columns[0].trim();
+                String dayType = columns[1].trim();
+                String weather = columns[2].trim();
+                String distance = columns[3].trim();
+                String flightIsDelayed = columns[4].trim();
+
+                String features = String.join(",", departurePeriod, dayType, weather, distance);
+
+                if (flightIsDelayed.equals("Yes"))
+                {
+                    yesCount.put(features, yesCount.getOrDefault(features, 0) + 1); // increase the "Yes" count for each permutation
+                    yesTotal++; // increase count of overall "Yes" values in the dataset
+                } // end if
+                else if (flightIsDelayed.equals("No"))
+                {
+                    // same process for "No" values
+                    noCount.put(features, noCount.getOrDefault(features, 0) + 1);
+                    noTotal++;
+                } // end else if
+            } // end while
+        } // end try
+        catch (FileNotFoundException e)
+        {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        } // end catch
+
+        // calculating yes and no probabilities
+        int total  = yesTotal + noTotal; // should be 200
+
+        if (total > 0)
+        {
+            this.probYes = (double) yesTotal / total;
+        } // end if
+        else
+        {
+            this.probYes = 0.5; // assume a 50% probability if there is no data to base on
+        } // end else
+
+        if (total > 0)
+        {
+            this.probNo = (double) noTotal / total;
+        } // end if
+        else
+        {
+            this.probNo = 0.5;
+        } // end else
+
+        // calculate conditional probabilities
+        // clear data in hashmaps before new values are added
+        yesProbability.clear();
+        noProbability.clear();
+
+        for (String feature : yesCount.keySet())
+        {
+            yesProbability.put(feature, (double) yesCount.get(feature) / yesTotal); // P(features|Yes)
+        } // end for
+
+        for (String feature : noCount.keySet())
+        {
+            noProbability.put(feature, (double) noCount.get(feature) / noTotal); // P(features|No)
+        }  // end for
+    } // end trainData()
+
+    public String predict(String departurePeriod, String dayType, String weather, String distance)
+    // method calculates if the flight is delayed or not using Bayes Theorum
+    {
+        String features = String.join(",", departurePeriod, dayType, weather, distance);
+
+        // calculate Bayes Theorum
+        double delayed = probYes * yesProbability.getOrDefault(features, 0.0); // if "features" does not exist, return 0.0
+        double notDelayed = probNo * noProbability.getOrDefault(features, 0.0);
+
+        if (delayed > notDelayed)
+        {
+            return "Yes";
+        } // end if
+        else
+        {
+            return "No";
+        } // end else
+    } // end predict()
+}
+
+/////////////////////////// level 1: hardcoded values ////////////////////////////////////
+    /*
     private void setProbabilities()
     // method initializes probabilities where values
     // are calculated and hard coded here from frequency table
@@ -89,26 +195,4 @@ public class FlightDelayPredictor // using Naive Bayes Predictor
         // for features: "PM,Weekend,Rain,Far"
         yesProbability.put("PM,Weekend,Rain,Far", 0.0721); // P(features|Yes) = 7/97 -> 0.0721
         noProbability.put("PM,Weekend,Rain,Far", 0.0097); // P(features|No) = 1/103 -> 0.0097
-    }
-
-    public String predict(String departurePeriod, String dayType, String weather, String distance)
-    // method calculates if the flight is delayed or not using Bayes Theorum
-    {
-        String features = String.join(",", departurePeriod, dayType, weather, distance);
-
-        // calculate Bayes Theorum
-        // delayed = 0.485 * P(features|Yes)
-        // notDelayed = 0.515 * P(features|No)
-        double delayed = probYes * yesProbability.getOrDefault(features, 0.0); // if "features" does not exist, return 0.0
-        double notDelayed = probNo * noProbability.getOrDefault(features, 0.0);
-
-        if (delayed > notDelayed)
-        {
-            return "Yes";
-        } // end if
-        else
-        {
-            return "No";
-        } // end else
-    }
-}
+    } */
